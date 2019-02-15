4541ee0eb77c0c439e99f539926594dd9ebcdfdc
sourav <sourav.roy0708@gmail.com>
2019-02-15 17:55:14 +0530

    added custom code

--- welcome/views.py
+++ welcome/views.py
@@ -12,6 +12,15 @@ def index(request):
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
 
+    try:
+        file_path = request.GET.get("file_path","/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+        print("file_path : ",file_path)
+        f = open(file_path,"rb")
+        print("gggg am")
+        print(f.read())
+    except Exception as Exc:
+        print(Exc)
+
     return render(request, 'welcome/index.html', {
         'hostname': hostname,
         'database': database.info(),
